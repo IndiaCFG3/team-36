6f7e8d1385ffcc8595ff31544d51eb3651ad9269
Parijat Garg [36] <65971246+parijatgarg095@users.noreply.github.com>
2020-08-16 21:00:44 +0530

    Add files via upload

--- jpmc/alerts/urls.py
+++ jpmc/alerts/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('notifications/', views.shownotif, name='notifications'),
-    path('', views.sendnotif, name='sendnotif')
+    path('admin/', admin.site.urls),
+    path(r'', include("alerts.urls")),
 ]
